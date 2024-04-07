8a8e8757c39e3c7526f61e1bf454c8a8bd37e5bd
Roman Shupak <romanshupak808@gmail.com>
2024-04-07 12:14:51 +0300

    solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views import generic
 from taxi.models import Driver, Car, Manufacturer
 
 
@@ -13,3 +13,37 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_list.html"
+    context_object_name = "manufacturer_list"
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
+
+
+class CarListView(generic.ListView):
+    model = Car
+    template_name = "taxi/car_list.html"
+    context_object_name = "car_list"
+    queryset = Car.objects.select_related("manufacturer")
+    paginate_by = 5
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+
+
+class DriverListView(generic.ListView):
+    model = Driver
+    template_name = "taxi/driver_list.html"
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
+    context_object_name = "drivers_detail"
+    queryset = Driver.objects.prefetch_related("cars")
